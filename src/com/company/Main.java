dc90f1e45a02d7c11f7ca9e0d17a03d86a70bd43
Simon Eriksen <simonffsss@gmail.com>
2021-09-08 09:54:50 +0200

    testing commit again

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,18 @@ package com.company;
 
 public class Main {
 
+    public static void plusThis(double a, double b){
+        double sumPlusThis = a + b;
+        System.out.println("Dette er plus funktionen." + "\n Plusser nu: " + a + " + " + b);
+
+    }
     public static void main(String[] args) {
 	//Test for first commit in a working repository
+        //okay vi skal lave en calculator, som gruppe hvor alle skal committe en metode.
         System.out.println("Hej med dig");
+        double a = 0;
+        double b = 0;
+        System.out.println("Vi prøver lige med nogle tal.");
+
     }
 }
