d114d8f4316ffa0fe7737e5f25b694fc83a3a52c
Simon <60150159+Simon205453@users.noreply.github.com>
2021-09-08 09:39:32 +0200

    Update from github test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	//Test for first commit in a working repository
-        System.out.println("Hej");
+        System.out.println("Hej med dig");
     }
 }
